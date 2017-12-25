2b9c5cb1570555a55da34f6929cebf458c3f8a4b
miqi1992 <1216938752@qq.com>
2017-12-25 16:04:20 +0800

    删除无效数据

--- python_Spider/chapter03/phantomJS/pageload.js
+++
@@ -1,9 +0,0 @@
-var page = require('webpage').create();
-
-page.open('http://www.cnblogs.com/miqi1992/', function(status){
-	console.log("Status: "+status);
-	if(status === "success"){
-		page.render('qiye.png');
-	}
-	phantom.exit()
-})
