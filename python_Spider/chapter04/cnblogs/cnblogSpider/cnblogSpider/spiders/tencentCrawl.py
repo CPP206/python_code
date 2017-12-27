14275484b708993c638780bb5eecdb99c2ea41a3
miqi1992 <1216938752@qq.com>
2017-12-27 20:22:38 +0800

    scrapy学习

---
+++ python_Spider/chapter04/cnblogs/cnblogSpider/cnblogSpider/spiders/tencentCrawl.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from cnblogSpider.items import TencentItem
+
+class TencentcrawlSpider(CrawlSpider):
+    name = 'tencentCrawl'
+    allowed_domains = ['hr.tencent.com']
+    start_urls = ['http://hr.tencent.com/position.php?&start=0#a']
+
+    rules = (
+        Rule(LinkExtractor(allow=(r'start=\d+')), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        content = response.xpath('//tr[@class="odd"]')
+        content += response.xpath('//tr[@class="even"]')
+
+        print(len(content))
+        for each in content:
+            item = TencentItem()
+            name = each.xpath('./td[1]/a/text()').extract()[0]
+            detailLink = each.xpath('./td[1]/a/@href').extract()[0]
+            positionInfo = each.xpath('./td[2]/text()').extract()[0]
+            peopleNumber = each.xpath('./td[3]/text()').extract()[0]
+            workLocation = each.xpath('./td[4]/text()').extract()[0]
+            publishTime = each.xpath('./td[5]/text()').extract()[0]
+
+            item['name'] = name.encode('utf-8')
+            item['detailLink'] = detailLink.encode('utf-8')
+            item['positionInfo'] = positionInfo.encode('utf-8')
+            item['peopleNumber'] = peopleNumber.encode('utf-8')
+            item['workLocation'] = workLocation.encode('utf-8')
+            item['publishTime'] = publishTime.encode('utf-8')
+
+            yield item
