daf2359996937f3fa699b3524ce3cf2175f6069b
miqi1992 <1216938752@qq.com>
2018-01-26 17:00:51 +0800

    python网络编程

---
+++ python高级/网络编程/udp_fuwuqi.py
@@ -0,0 +1,23 @@
+﻿# -*- coding:utf-8 -*-
+
+import socket
+import time
+import threading
+
+
+def udplink(sock, data, addr):
+	print("Received from %s:%s" % addr)
+	reply = 'Hello,%s!' % data.decode('utf-8')
+	time.sleep(1)
+	sock.sendto(reply.encode('utf-8'), addr)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.bind(('127.0.0.1', 9999))
+
+while True:
+	data, addr = s.recvfrom(1024)
+	threading.Thread(target=udplink, args=(s, data, addr))
+
+
